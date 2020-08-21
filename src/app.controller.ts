42d96e930aca324195c4be55f1be61cfcd20f826
Stefan Vucan <stefan.vucan.16@singimail.rs>
2020-08-21 15:24:38 +0200

    App controller changes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,13 +3,8 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  @Get()
-  getHello(): string {
-    return 'Hello world!';
-  }
-  
-  @Get('world')
-  getWorld(): string {
-    return 'World!';
+  @Get() 
+  getIndex(): string {
+    return 'Home page!';
   }
 }
